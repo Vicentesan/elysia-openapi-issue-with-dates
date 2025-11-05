ed82c9ebd8809b86b3b279cebd420e6d82e78740
Vicentesan <vikom.sanchez@gmail.com>
2025-11-05 18:29:40 -0300

    chore: add more details to the issue

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ new Elysia()
 			},
 		}),
 	)
-	// this throw an error
 	.get(
 		"/error/hello/:name",
 		({ params, status }) => {
@@ -22,10 +21,37 @@ new Elysia()
 			});
 		},
 		{
+			// Issue: z.date() does not properly serialize to OpenAPI schema
+			// The response schema is not visible in the generated OpenAPI documentation
+			// because z.date() doesn't translate well to JSON Schema format
 			response: {
 				200: z.object({
 					name: z.string(),
 					date: z.date(),
+				}),
+			},
+		},
+	)
+	.get(
+		"/hello/:name",
+		({ params, status }) => {
+			const date = new Date();
+
+			return status(200, {
+				name: params.name,
+				// TypeScript error: Type 'Date' is not assignable to type 'string'
+				// z.iso.date() expects an ISO date string, but we're passing a Date object
+				date,
+			});
+		},
+		{
+			// Solution: z.iso.date() properly serializes to OpenAPI schema as a string format
+			// The response schema is correctly displayed in the OpenAPI documentation
+			// However, runtime values must be ISO date strings, not Date objects
+			response: {
+				200: z.object({
+					name: z.string(),
+					date: z.iso.date(),
 				}),
 			},
 		},
